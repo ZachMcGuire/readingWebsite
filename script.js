033e5acd5d03c15bae823f5678028a553116a809
ZachMcGuire <zach.mcguire09@gmail.com>
2024-01-26 16:37:46 -1000

    Added HTML text, CSS styling, and started javaScript logic

---
+++ script.js
@@ -0,0 +1,16 @@
+// const readLine = require("readLine");
+// const f = require("fs");
+
+// let file = "/ball.txt"
+//  let rl = readLine.createInterface({
+//     input : f.createReadStream(file),
+//     output : process.stdout,
+//     terminal : false
+
+//  });
+
+//  rl.on('line', function (text) {
+//     console.log(text);
+//  })
+
+console.log("Test")
